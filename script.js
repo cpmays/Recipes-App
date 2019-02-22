9777c3c11ddbb05972116f149e27d27d4f411b1b
cpmays <43414012+cpmays@users.noreply.github.com>
2019-02-22 08:44:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿
+function myFunction()
+	{
+    var x = document.getElementById("myNav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    }
+	else
+	{
+        x.className = "navbar";
+    }
+}
